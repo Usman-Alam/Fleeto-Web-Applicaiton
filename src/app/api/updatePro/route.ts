d36f249e33d358f51c745febc711f153fc6d8381
SyedSafiullahK <115879169+SyedSafiullahK@users.noreply.github.com>
2025-05-04 03:22:57 +0500

    Update route.ts

--- src/app/api/updatePro/route.ts
+++ src/app/api/updatePro/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import connectDB from "../../../../server/server";
-import User from "@/models/user";
+import User from "../../../models/user";
 
 export async function POST(req: Request) {
     try {
